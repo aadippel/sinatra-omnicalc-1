0b1c39c151484cc088f85746250e9b0a1d2aea50
aadippel <aadippel@gmail.com>
2025-02-02 02:03:43 +0000

    added remaining erbs and fixed code

--- app.rb
+++ app.rb
@@ -14,9 +14,9 @@ get("/square/new") do
 end
 
 get("/square/results") do
-  @the_num = params.fetch("users_number").to_i
+  @square_num = params.fetch("users_number").to_i
 
-  @the_result = @the_num.to_f ** 2 # ** is the exponentiation operator
+  @square_result = @square_num.to_f ** 2 # ** is the exponentiation operator
 
   erb(:square_results)
 end
@@ -25,6 +25,38 @@ get("/square_root/new") do
   erb(:new_square_root_calc)
 end
 
+get("/square_root/results") do
+  @square_root_num = params.fetch("users_number").to_i #correct
+
+  @square_result = Math.sqrt(@square_root_num).to_f
+
+  erb(:square_root_results)
+end
+
 get("/payment/new") do
   erb(:new_payment_calc)
+end
+
+get("/payment/results") do
+  @apr_num = @apr_num..to_fs(:percentage, {:precision => 4}) #correct
+
+  @years_num = @years_num.to_i #correct
+
+  @principal_num = @principal_num.to_fs #correct
+
+  erb(:square_root_results)
+end
+
+get("/random/new") do
+  erb(:new_random_calc)
+end
+
+get("/random/results") do
+  @minimum_num = @minimum_num.to_f #correct
+
+  @maximum_num = @maximum_num.to_f #correct
+
+  @random_result = rand(@minimum_num..@maximum_num).to_fs(:precision => 15) #correct
+
+  erb(:random_results)
 end
